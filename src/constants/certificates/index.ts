d13726442688d20258aadf058129d91160c35f96
dragaus <dragalichi@gmail.com>
2022-01-26 21:18:30 -0600

    Add certifies

--- src/constants/certificates/index.ts
+++ src/constants/certificates/index.ts
@@ -1,6 +1,9 @@
 import { CertificateData } from "../../utils/types";
 import QYDTFWXRTHDR from "../../assets/images/certificates/QYDTFWXRTHDR.png";
 import RLRZGTM9AA4Z from "../../assets/images/certificates/RLRZGTM9AA4Z.png";
+import EEY7GFJQXQEA from "../../assets/images/certificates/EEY7GFJQXQEA.png";
+import LXEL4VCMAPQS from "../../assets/images/certificates/LXEL4VCMAPQS.png";
+import SZM2HKBER2C7 from "../../assets/images/certificates/SZM2HKBER2C7.png";
 
 const udemyLink = (id: string) => `https://www.udemy.com/certificate/${id}`;
 const udemyImage = (id: string) =>
@@ -54,19 +57,19 @@ export const certificates: CertificateData[] = [
   },
   {
     certificateLink: courseraLink("SZM2HKBER2C7"),
-    imageLink: courseraImage("SZM2HKBER2C7"),
+    imageLink: SZM2HKBER2C7,
     courseName: "Ruby on Rails: An Introduction",
     finishDate: "06/2020",
   },
   {
     certificateLink: courseraLink("EEY7GFJQXQEA"),
-    imageLink: courseraImage("EEY7GFJQXQEA"),
+    imageLink: EEY7GFJQXQEA,
     courseName: "Introduction to XR: VR, AR, and MR Foundations",
     finishDate: "06/2020",
   },
   {
     certificateLink: courseraLink("LXEL4VCMAPQS"),
-    imageLink: courseraImage("LXEL4VCMAPQS"),
+    imageLink: LXEL4VCMAPQS,
     courseName: "Introduction to Game Development",
     finishDate: "06/2020",
   },
